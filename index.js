f2e16634b7fd3e267d851eb373e8063c91a2f91b
shuvasish <shuvasishtalukdershuvoalways@gmail.com>
2021-03-24 02:23:13 +0600

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.json());
 // console.log(express);
 // console.log(app);
 
-const rootCall = (req, res) => res.send("thank you very ");
+const product = {
+  name: "table",
+  price: 178,
+  location: "sunamganj",
+};
+
+const users = ["shuvasish", "dipu", "supto", "topu", "prottoy", "joy", "rakib"];
+
+const rootCall = (req, res) => res.send(product);
 app.get("/", rootCall);
 
-app.listen(3000, console.log("listening to port 3000"));
+app.get("/fruits/banana", (req, res) => {
+  res.send({ name: "banana", price: 22, quantity: 3 });
+});
+
+app.get("/users/:id", (req, res) => {
+  //   const re = JSON.parse(res);
+  const id = req.params.id;
+  const name = users[id];
+  res.send({ id, name });
+  //   console.log(req.params.id);
+  console.log(req.query);
+});
+
+//post request
+
+app.post("/addUser", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(4000, console.log("listening to port 4000"));
